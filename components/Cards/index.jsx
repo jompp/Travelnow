90126a2c6ef42f18f15672e1d1cfe47eb4f80841
João Chaves <jomp@pop-os.localdomain>
2022-07-13 00:20:02 -0300

    alterando importação da imagem das estrelas de avaliação dos cards

--- components/Cards/index.jsx
+++ components/Cards/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './cards.css'
+import starImg from '../../images/stars.svg'
 
 export default function Cards(props) {
     return (
@@ -7,7 +8,7 @@ export default function Cards(props) {
             <img className='card-img' src={props.img} alt={props.alt} />
             <div className='card-info'>
                 <h3>{props.title}</h3>
-                <img src="../images/stars.svg" alt="5 stars" />
+                <img src={starImg} alt="5 stars" />
                 <span>$6000</span>
             </div>
         </div>
